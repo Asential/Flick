b0c551602229e6f5849fc716e7051c37ca39fd2b
Asential <50661902+Asential@users.noreply.github.com>
2020-07-03 07:35:33 +0530

    Add files via upload
    Bug-Fixed Update

--- application.py
+++ application.py
@@ -1,37 +1,141 @@
 import os
-import datetime
+import collections
+from datetime import datetime
+import json
 from flask import Flask, render_template, redirect, jsonify, request, url_for, session
-from flask_socketio import SocketIO, emit, send,join_room, leave_room
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
 from flask_session import Session
-
+from json import JSONEncoder
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 socketio = SocketIO(app)
 Session(app)
-channels = []
 
-channels={"general": []}
-# channels['General']=[] 
-# list of all channels except General
-# channelsList=[]
-# privateMessages={}
-# usersList={}     
-# limit=100
+usernames=[]
+channelsList=[]
+channelNames=[]
+currentID = 0
+global currentMessageID
+currentMessageID = 0
+
+class Channels():
+     def __init__(self, name, messages, channelid):
+         self.name = name
+         self.messages = messages
+         self.channelid = channelid
+
+class Message():
+     def __init__(self, content, user, time):
+         self.content = content
+         self.user = user
+         self.time = time
+
+
+class L(list):
+     def append(self, item):
+         list.append(self, item)
+         if len(self) > 100: del self[0]
+
+class MessageEncoder(JSONEncoder):
+        def default(self, o):
+            return o.__dict__
+
+general = Channels('General', [], len(channelsList))
+general.messages = L()
+channelsList.append(general)
+channelNames.append(general.name)
 
 @app.route('/')
-def index():
-    return render_template("index.html", channel = channels)
+def user():
+    global currentID
+    if 'username' in session:
+        return redirect(url_for('channels', channel_id=currentID))
+    return render_template("user.html")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    global currentID
+    username = request.form["username"]
+    if username in usernames or username == '':
+        return redirect(url_for('/'))
+    usernames.append(username)
+    session['username'] = username
+    return redirect(url_for('channels', channel_id=currentID))
+    
+@app.route('/channels/<int:channel_id>')
+def channels(channel_id):
+    global currentID 
+    currentID = channel_id
+    if session['username'] == None:
+        redirect(url_for('user'))
+    username = session['username']
+    socketio.emit('message id', channel_id)
+    return render_template("channel.html", channelsList = channelsList, currentChannelID = channel_id, username = username)
+
+@app.route('/logout.html', methods=['POST'])
+def logout():
+    user = session['username']
+    usernames.remove(user)
+    session.pop('username', None)
+    return redirect(url_for('user'))
+
+@socketio.on('channel')
+def channel(name):
+    global currentID 
+
+    if name == '':
+        return "Channel Can't be empty"
+    for i in channelNames:
+        if name == i:
+            return "Channel already exists"
+    channel = Channels(name, [], len(channelsList))
+    channel.messages = L()
+    channelsList.append(channel)
+    channelNames.append(channel.name)
+    emit('current channel', currentID)
+    emit('change list', channelNames, broadcast=True)
 
 @socketio.on('message')
-def handleMessage(msg):
+def message(msg):
+    # global currentID
+    global currentMessageID
+    messageID = currentMessageID
+    username = session['username']
     
-    print('Message: ' + msg)
-    channels["general"].append(msg)
-    print(channels)
-    emit("channel", channels, broadcast=True)
+    for i in channelsList:
+        if i.channelid == int(messageID):
+            currentChannel = i.messages
+    dateTimeObj = datetime.now()
+    ts = str(dateTimeObj.day) + '/' + str(dateTimeObj.month) + '/' + str(dateTimeObj.year)  + ' (' + str(dateTimeObj.hour) + ':' + str(dateTimeObj.minute) + ')'
+    newMessage = Message(msg, username, ts)
+    currentChannel.append(newMessage)
+    messageJSONData = json.dumps(currentChannel, cls=MessageEncoder)
+    emit('message list', messageJSONData, broadcast=True)
+    emit('scroll bottom')
 
+@socketio.on('delete message')
+def delete(messageData):
+    global currentMessageID
+    messageID = currentMessageID
+    username = session['username']
 
+    for i in channelsList:
+        if i.channelid == int(messageID):
+            currentChannel = i.messages 
+    for m in currentChannel:
+        checker = str(m.user)+str(m.content)+str(m.time)
+        if checker == messageData:
+            currentChannel.remove(m)
+    emit('message channel', int(currentMessageID), broadcast=True)
+    messageJSONData = json.dumps(currentChannel, cls=MessageEncoder)
+    emit('message list', messageJSONData, broadcast=True)
+
+@socketio.on('message id')
+def setMessageID(data):
+    global currentMessageID
+    currentMessageID = data
+    emit('message channel', int(currentMessageID), broadcast=True)
--- static/index.js
+++ static/index.js
@@ -1,34 +1,159 @@
-$(document).ready(function() {
 
-	var socket=io.connect(location.protocol+'//'+document.domain+':'+location.port);
-	myStorage = window.localStorage;
-	socket.on('connect', function() {
-		// socket.send(person + ' has connected!');
-		// document.querySelectorAll('button').forEach(button => {
-            // button.onclick = () => {
-        //         const selection = button.dataset.vote;
-        //         socket.emit('submit vote', {'selection': selection});
-        //     };
-        // });
-		$('#sendbutton').on('click', function() {
-			if ($('#myMessage').val() == "" ){
-					return
-			}
-			socket.emit('message', $('#myMessage').val());
-			$('#myMessage').val('');
-		});
+document.addEventListener('DOMContentLoaded', () => {
+	
+	$('body').on('click', '.delete', function(){
+		console.log("DELETE FUNCTION")
+		socket.emit('message id',$('#currentChannelID').html());
+		var $this = $(this);
+		var t = $this.text();
+		$this.html(t.replace('&lt','<').replace('&gt', '>'));
+		socket.emit('delete message', t)
 	});
 
-    socket.on('channel', data => {
-		console.log(data)
-		// document.querySelector('#messages').innerHTML = data.general;
-		let str = "<ul>" 
-
-		for(let i = 0; i < data.general.length; i++){
-			str += "<li>" + data.general[i] + "</li>"
+	// Connect to websocket
+	var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+	window.localStorage
+	var currentUser;
+	var currentChannel;
+	var currentMessage;
+	
+	// When connected, configure buttons
+	
+		
+	socket.on('connect', () => {
+		
+		currentUser = $("#user").first().text();
+		console.log(currentUser);
+		$('#scrolltobottomButton').on('click', function(){
+			$('#messages').scrollTop(50000);
+		});
+		$("#channelButton").on('submit click',function(e){
+			e.preventDefault();
+			socket.emit('channel', $('#myChannel').val());
+			$('#myChannel').val('');
+		});
+		$("#messageButton").on('submit click',function(e){
+			if ($('#myMessage').val() == ''){
+				e.preventDefault();
+				// alert("Messaege can't be empty!")
+			}
+				
+			else{
+				e.preventDefault();
+				socket.emit('message id',$('#currentChannelID').html());
+				socket.emit('message', $('#myMessage').val());
+			}
+			$('#myMessage').val('');
+		});
+		if (localStorage.getItem("channelid")) {
+			window.location.replace(`/channels/${localStorage.getItem("channelid")}`);
 		}
-		str += "</ul>"
-		document.querySelector('#messages').innerHTML = str
-    });
+		
+	});
 
+	socket.on('current user', data => {
+		currentUser = data
+	})
+
+	socket.on('message channel', data => {
+		currentMessage = data
+	})
+
+	currentChannel = $('#currentChannelID').html();
+
+	// When a new vote is announced, add to the unordered list
+	socket.on('change list', data => {
+		var str = '';
+		console.log(currentChannel)
+		for(let i in data){
+			if(i == currentChannel){
+				str += '<a href="/channels/'+ i + '"class="hvr-bubble-float-right lnk">'+
+				'<li class="list-group-item chatspace mrgn activechannel">' + data[i] + '</li>' + 
+				'</a>'
+			}
+			else{
+				str += '<a href="/channels/'+ i + '"class="hvr-bubble-float-right lnk">'+
+				'<li class="list-group-item chatspace mrgn" style="color: black">' + data[i] + '</li>' + 
+				'</a>'
+			}
+			
+		}
+		$("#channels").html(str);
+		location.reload(true);
+	});
+		socket.on('message list', data => {
+			console.log(currentMessage)
+			console.log(currentChannel)
+			if(currentChannel == currentMessage){
+				var obj = JSON.parse(data);
+			var str = ''	
+			for(i in obj){
+				if(obj[i].user == currentUser){
+					str += '<div class="container chatspace">' + 
+					'<div class="row">' + 
+					'<div class="col-7" style="width: 66.66%;">' + 
+					'</div>' +
+					'<li class="list-group-item col-5 usermsg" style="width: 33.33%;">' + 
+					'<span id="sent" style="float: right;">' + obj[i].user + '</span>' + '<br>' + 
+					'<span style="float: left; padding-left: 15px; line-height: 2;">' + obj[i].content  + '</span>' + '<br><br>' + 
+					'<div class="col-12 timestampSent">' + obj[i].time + '</div>' + 
+					'<div style="float: right;">' + 
+					'<button class="delete" style="padding: 5px; background: red">' + 
+					'<p style="display: none;">' + obj[i].user + obj[i].content + obj[i].time + 
+					'</p>' + 
+					'</button>' + 
+					'</div>' +
+					'</li>' + 
+					'</div>' + 
+					'</div>'
+				}
+				else{
+					str += '<div class="container chatspace">' + 
+					'<div class="row">' + 
+					'<li class="list-group-item col-5 sendermsg" style="width: 33.33%;">' + 
+					'<span id="recieved">' + obj[i].user + '</span>' + '<br>' + 
+					'<span style="line-height: 2;">' + obj[i].content  + '</span>'+ '<br><br>' + 
+					'<div class="col-12 timestampRecieved">' + obj[i].time + '</div>' + 
+					'</li>' + 
+					'<div class="col-7" style="width: 66.66%;">' + 
+					'</div>' +
+					'</div>' +
+					'</div>'
+				}
+			}
+			$("#messages").html(str);
+			$('#messages').scrollTop(50000);
+			}
+		});
+
+		
+		socket.on('scroll bottom', data =>{
+			$('#messages').scrollTop(50000);
+		})
+	
+		
+		// function offset(el) {
+		// 	var rect = el.getBoundingClientRect(),
+		// 	scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+		// 	scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		// 	return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+		// }
+	
+		
+		
+		// function scrollChannel{
+		// 	var div = document.querySelector('.activechannel');
+		// 	var divOffset = offset(div);
+		// 	console.log(divOffset.left, divOffset.top);
+		// 	$('#channels').scrollTop(divOffset.top);
+		// }
+	
 });
+                                         
+                                   
+                                
+                                    
+                                       
+                                       
+                                        
+                                            
